740ba74055f4759416ccf73b77e22ecfd9972aa8
sungwoo0902 <rhtjddn2@gmail.com>
2023-11-10 20:48:09 +0900

    스프링 핵심 원리 강의 - 기본편 색션 9 빈 스코프 실습

--- Spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -9,13 +9,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // basePackages = "hello.core.member",
-        // basePackageClasses =AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
-
+/*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+ */
 }
--- Spring/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ Spring/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotiation.MainDeiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@MainDeiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
